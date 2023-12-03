28a8d1c9caafd251941a06379e3e732e5c5794ac
retgal <galante@puzzle.ch>
2023-12-03 20:50:41 +0100

    Improving robustness

--- mayer_osiris.sh
+++ mayer_osiris.sh
@@ -3,19 +3,18 @@
 workingDir=/home/bot/balancer
 scriptName=mayer.py
 
-cd ${workingDir}
-find -name "*.mid" -type f 2>/dev/null | while read file; do
-  read pid instance <${file}
-  kill -0 ${pid} 2>/dev/null
-  if [ $? -eq 1 ]; then
-    echo resurrecting ${instance}
-    tmux has-session -t ${instance} 2>/dev/null
+cd "${workingDir}" || exit 1
+find . -name "*.mid" -type f 2>/dev/null | while read -r file; do
+  read -r pid instance <"${file}"
+  if kill -0 "${pid}" 2>/dev/null; then
+    echo "${instance}" is alive
+  else
+    echo resurrecting "${instance}"
+    tmux has-session -t "${instance}" 2>/dev/null
     if [ $? -eq 1 ]; then
-      tmux new -d -s ${instance}
+      tmux new -d -s "${instance}"
     fi
     sleep 2
-    tmux send-keys -t "$instance" C-z "$workingDir/$scriptName $instance" C-m
-  else
-    echo ${instance} is alive
+    tmux send-keys -t "${instance}" C-z "${workingDir}/${scriptName} ${instance}" C-m
   fi
 done
