405ce4aece43e1b0d5481691ba7806b4a88f9299
retgal <galante@puzzle.ch>
2024-10-07 20:26:57 +0200

    Check process name

--- mayer_osiris.sh
+++ mayer_osiris.sh
@@ -3,18 +3,26 @@
 workingDir=/home/bot/balancer
 scriptName=mayer.py
 
+resurrect() {
+  instance=$1
+  echo "resurrecting ${instance}"
+  if ! tmux has-session -t "${instance}" 2>/dev/null; then
+    tmux new -d -s "${instance}"
+    sleep 1
+  fi
+  tmux send-keys -t "${instance}" C-z "${workingDir}/${scriptName} ${instance}" C-m
+}
+
 cd "${workingDir}" || exit 1
-find . -name "*.mid" -type f 2>/dev/null | while read -r file; do
+find . -name "*.mid" -type f 2>/dev/null | while read -r file;
+do
   read -r pid instance <"${file}"
   if kill -0 "${pid}" 2>/dev/null; then
-    echo "${instance}" is alive
-  else
-    echo resurrecting "${instance}"
-    tmux has-session -t "${instance}" 2>/dev/null
-    if [ $? -eq 1 ]; then
-      tmux new -d -s "${instance}"
+    processName=$(ps --pid "${pid}" -o comm h)
+    if [ "${scriptName}" = "${processName}" ]; then
+      echo "${instance} is alive"
+      continue
     fi
-    sleep 2
-    tmux send-keys -t "${instance}" C-z "${workingDir}/${scriptName} ${instance}" C-m
   fi
+  resurrect "${instance}"
 done
